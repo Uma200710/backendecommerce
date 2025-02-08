50d64023289e9d334fa53811ea8d7f18a5085cf5
UmaAudaGiori <159306386+Uma200710@users.noreply.github.com>
2025-02-08 19:35:22 +0000

    first commit

--- routes/products.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getProducts, createProduct } from '../controllers/productsController.js';
-
-const router = express.Router();
-
-router.get('/', getProducts);  // Obtener todos los productos
-router.post('/', createProduct);  // Crear un nuevo producto
-
-export default router;
